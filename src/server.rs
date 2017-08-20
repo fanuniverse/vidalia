dbd7d86dd168c1070267425def018d92e68bb5e9
little-bobby-tables <little-bobby-tables@users.noreply.github.com>
2017-08-20 21:13:45 +0700

    retrieve image blob from multipart request

--- src/server.rs
+++ src/server.rs
@@ -4,6 +4,7 @@ use iron::status;
 use iron::request::Body;
 
 use multipart::server::{Multipart};
+use std::io::Read;
 
 pub fn run() {
     Iron::new(serve)
@@ -15,8 +16,7 @@ fn serve(req: &mut Request) -> IronResult<Response> {
     match Multipart::from_request(req) {
         Ok(data) => {
             match parse_multipart(data) {
-                Ok(manifest) => {
-                    println!("{:?}", manifest);
+                Ok(res) => {
                     Ok(Response::with((status::Ok, "ok")))
                 }
                 Err(err) => {
@@ -30,14 +30,30 @@ fn serve(req: &mut Request) -> IronResult<Response> {
     }
 }
 
-fn parse_multipart(mut data: Multipart<&mut Body>) -> Result<String, &'static str> {
+fn parse_multipart(mut data: Multipart<&mut Body>) -> Result<(String, Vec<u8>), &'static str> {
     let mut data_manifest = None;
+    let mut data_image_blob = None;
 
-    data.foreach_entry(|field| {
+    data.foreach_entry(|mut field| {
         if let "manifest" = field.name.as_str() {
             data_manifest = field.data.as_text().map(|s| s.to_owned());
         }
+        if let "image" = field.name.as_str() {
+            let mut buf = Vec::new();
+
+            let fully_read = field.data.as_file()
+                .map(|f| f.read_to_end(&mut buf).is_ok())
+                .unwrap_or(false);
+
+            if fully_read { data_image_blob = Some(buf); }
+        }
     }).or_else(|_| Err("Malformed multipart request"))?;
 
-    data_manifest.ok_or("No manifest is provided in the request")
+    data_manifest
+        .ok_or("Request parameter (\"manifest\") is missing or malformed")
+        .and_then(|manifest| {
+            data_image_blob
+                .ok_or("Request parameter (\"image\") is missing or malformed")
+                .map(|blob| (manifest, blob))
+        })
 }
