22478d9b34e42c034cb24fff30c8c85ef199ed10
little-bobby-tables <little-bobby-tables@users.noreply.github.com>
2017-08-22 22:20:26 +0700

    main.rs -> lib.rs (for integration tests)

---
+++ src/lib.rs
@@ -0,0 +1,26 @@
+extern crate iron;
+extern crate multipart;
+
+extern crate serde;
+extern crate serde_json;
+#[macro_use] extern crate serde_derive;
+
+extern crate magick_rust;
+
+pub mod types;
+mod server;
+mod transforms;
+
+use types::{Manifest, ProcessingResult};
+
+pub fn run() {
+    transforms::init();
+
+    server::run(process);
+}
+
+fn process(manifest: Manifest, blob: Vec<u8>) -> Result<ProcessingResult, &'static str> {
+    let images = transforms::run(&manifest, &blob)?;
+
+    Ok(ProcessingResult { images: images })
+}
--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,5 @@
-extern crate iron;
-extern crate multipart;
-
-extern crate serde;
-extern crate serde_json;
-#[macro_use] extern crate serde_derive;
-
-extern crate magick_rust;
-
-pub mod types;
-mod server;
-mod transforms;
-
-use types::{Manifest, ProcessingResult};
+extern crate vidalia;
 
 fn main() {
-    transforms::init();
-
-    server::run(do_stuff);
-}
-
-fn do_stuff(manifest: Manifest, blob: Vec<u8>) -> Result<ProcessingResult, &'static str> {
-    let images = transforms::run(&manifest, &blob)?;
-
-    Ok(ProcessingResult { images: images })
+    vidalia::run();
 }
